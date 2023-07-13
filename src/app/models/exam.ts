bd90759086c8a4f990f8836f1e2e2ceb63576d64
jtspinelli <contato@jonathanspinelli.com>
2023-07-13 17:04:56 -0300

    feat: add models (types) 'Address', 'Exam' e 'Patient'

---
+++ src/app/models/exam.ts
@@ -0,0 +1,11 @@
+export type Exam = {
+  id: number;
+  name: string;
+  date: string;
+  time: string;
+  type: string;
+  laboratory: string;
+  url: string;
+  result: string;
+  status: boolean;
+}
