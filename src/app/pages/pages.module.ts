8e3066beda27acf5e3988c9894fdd210acc804c4
Saull Brandão <saull@outlook.com>
2023-07-25 11:37:52 -0300

    feat: display patient's history in chronological order

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { AgePipe } from '../pipes/age.pipe';
 import { MedicalRecordComponent } from './medical-record/medical-record.component';
+import { OrderByDateTimePipe } from '../pipes/order-by.pipe';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { MedicalRecordComponent } from './medical-record/medical-record.componen
     ComponentsModule,
     RouterModule,
     AgePipe,
+    OrderByDateTimePipe,
   ],
   providers: [provideNgxMask(), DatePipe],
 })
