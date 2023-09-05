a0676f033a1d72c2c1039822816eea7accfa991b
Saull Brandão <saull@outlook.com>
2023-09-05 09:18:33 -0300

    feat: adjust the elements on the sidebar and navbar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { SelectPatientModalComponent } from './search-patient-modal/select-patient-modal.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -11,13 +11,13 @@ import { RouterModule } from '@angular/router';
     ConfirmModalComponent,
     SelectPatientModalComponent,
     ToolbarComponent,
-    MenuLateralComponent,
+    SidebarComponent,
   ],
   exports: [
     ConfirmModalComponent,
     SelectPatientModalComponent,
     ToolbarComponent,
-    MenuLateralComponent,
+    SidebarComponent,
   ],
   imports: [CommonModule, RouterModule],
 })
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserType } from 'src/app/enums/user-type';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  readonly menuCategories = [
+    { title: 'início', route: '/', icon: 'house-fill' },
+    { title: 'prontuários', route: 'prontuarios', icon: 'person-lines-fill' },
+    {
+      title: 'paciente',
+      route: 'pacientes',
+      icon: 'person-fill',
+    },
+    {
+      title: 'medicamento',
+      route: 'medicamentos',
+      icon: 'capsule',
+    },
+    {
+      title: 'exercício',
+      route: 'exercicios',
+      icon: 'bicycle',
+    },
+    {
+      title: 'dieta',
+      route: 'dietas',
+      icon: 'card-list',
+    },
+    {
+      title: 'consulta',
+      route: 'consultas',
+      icon: 'calendar2-event',
+      userTypes: [UserType.ADMIN, UserType.MEDICO],
+    },
+    {
+      title: 'exame',
+      route: 'exames',
+      icon: 'activity',
+      userTypes: [UserType.ADMIN, UserType.MEDICO],
+    },
+    {
+      title: 'Listagem de registros',
+      route: 'registros',
+      icon: 'list-ul',
+      userTypes: [UserType.ADMIN],
+    },
+    {
+      title: 'Cadastrar usuário',
+      route: 'cadastrar',
+      icon: 'person-fill-add',
+      userTypes: [UserType.ADMIN],
+    },
+  ];
+
+  confirmMessage = 'Deseja realmente sair?';
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  shouldDisplay() {
+    return !this.router.isActive('/login', {
+      paths: 'exact',
+      queryParams: 'exact',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  isAdmin() {
+    return this.authService.isAdmin();
+  }
+
+  isDoctor() {
+    return this.authService.isDoctor();
+  }
+
+  userHasPermission(userTypes: UserType[] | undefined) {
+    if (!userTypes || this.isAdmin()) {
+      return true;
+    }
+
+    if (this.isDoctor()) {
+      return userTypes.includes(UserType.MEDICO);
+    }
+
+    return false;
+  }
+}
